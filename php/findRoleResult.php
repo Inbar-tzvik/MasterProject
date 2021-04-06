5e2006964f80e866b59739a8edf6d6bf53279856
mayco15 <maycohen155@gmail.com>
2021-04-06 11:42:07 +0300

    5

---
+++ php/findRoleResult.php
@@ -0,0 +1,33 @@
+
+<?php
+$server_name = "localhost";
+$username = "maycois";
+$password = "WwSVtsJ1pMl6";
+$database = "maycois_MasterProject";
+
+
+$conn = new mysqli($server_name, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection successful";
+
+
+// $sql ="SELECT SUM(html), FROM roles1";
+// $retval = mysql_query( $sql, $conn );
+
+
+
+
+
+
+
+// $conn->query($sql);
+if ($conn->query($sql) == FALSE) {
+    echo "Can not add new reservation. Error is: " .
+        $conn->error;
+    exit();
+}
+
+?>
