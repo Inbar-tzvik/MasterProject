3627b11026acc13b17f6ab718507d561c1dd4642
liatro136 <z4liatro1994@gmail.com>
2021-04-07 19:48:20 +0300

    מי

--- js/users.js
+++ js/users.js
@@ -2,11 +2,11 @@
 function myFunction() {
     var x, text;
   
-     var  emailErr = subjectErr = commentErr =true;
+    //  var  emailErr = subjectErr = commentErr =true;
      
      
       //  אימייל
-  
+      x = document.getElementById("email").value;
       var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
       if(!email.value.match(mailformat))
       {
@@ -19,8 +19,9 @@ function myFunction() {
       
     
     //  subject
-  
+    x = document.getElementById("subject").value;
      var s_letters = /^[A-Za-z]+$/;
+
      if(!subject.value.match(s_letters)){
         text = "You entered an invalid subject, the subject must contain only letters and spaces";
         document.getElementById("subjectErr").innerHTML = text;  
@@ -28,8 +29,8 @@ function myFunction() {
        }
 
         // comment
+        x = document.getElementById("comment").value;
       var letters = /^[A-Za-z]+$/;
-  
       
       if(!comment.value.match(letters)){
         text = "You entered an invalid comment, the comment must contain only letters and spaces";
@@ -42,20 +43,26 @@ function myFunction() {
     
      
             
-        if((emailErr && subjectErr &&  commentErr ) === true) {
+      //   if((emailErr && subjectErr &&  commentErr ) === true) {
           
          
-              alert('The form has been sent successfully');
-                   return true ;
-      }
+      //         alert('The form has been sent successfully');
+      //              return true ;
+      // }
         
      
-        else{
-               return false ;
+      //   else{
+      //          return false ;
                  
                 
-            } 
-      }
+      //       } 
+
+            
+    else{
+      alert('The form has been sent successfully');
+     }   
+     
+  }
   
   
   
