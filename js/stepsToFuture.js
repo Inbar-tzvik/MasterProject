d7e24b190e438e02c215ebfd7f6e7b1a95850edd
mayco15 <maycohen155@gmail.com>
2021-04-19 16:06:55 +0300

    jff

---
+++ js/stepsToFuture.js
@@ -0,0 +1,35 @@
+function myFunction() {
+
+   
+    // var  manageentErr= true
+    // var designErr= true
+    // var softwareErr= true
+    // var workInATeamErr= true 
+    // var umlDiagramOrientedErr= true
+    // var teachingErr= true 
+    // var analysisCapabilitiesErr =true;
+
+
+
+
+
+
+
+
+
+         
+//     if((emailErr && subjectErr &&  commentErr ) === true) {
+          
+         
+//         alert('The form has been sent successfully');
+//              return true ;
+// }
+ 
+
+//   else{
+//         return false ;
+          
+         
+    } 
+  
+}
