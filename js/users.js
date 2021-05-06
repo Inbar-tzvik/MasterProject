96133c7121a5c9fed1d3dae8e0ada8ce16b39d70
mayco15 <maycohen155@gmail.com>
2021-05-06 19:11:19 +0300

    Update users.js

--- js/users.js
+++ js/users.js
@@ -65,4 +65,77 @@ function myFunction() {
   
   
   
+  // Defining a function to display error message
+//function printError(elemId, hintMsg) {
+  //  document.getElementById(elemId).innerHTML = hintMsg;
+//}
+
+// Defining a function to validate form 
+function validateForm() {
+  // Retrieving the values of form elements 
+  var subject = document.contactForm.subject.value;
+  var email = document.contactForm.email.value;
+  var comment = document.contactForm.comment.value;
+
   
+// Defining error variables with a default value
+  var  emailErr = subjectErr = commentErr = true;
+  
+  
+  // Validate email address
+  if(email == "") {
+      printError("emailErr", "Please enter your email address");
+  } else {
+      // Regular expression for basic email validation
+      var regex = /^\S+@\S+\.\S+$/;
+      if(regex.test(email) === false) {
+          printError("emailErr", "Please enter a valid email address");
+      } else{
+          printError("emailErr", "");
+          emailErr = false;
+      }
+  }
+  
+  
+  // Validate subject
+  if(subject == "") {
+      printError("subjectErr", "Please enter your name");
+  } else {
+      var regex = /^[a-zA-Z\s]+$/;                
+      if(regex.test(subject) === false) {
+          printError("subjectErr", "Please enter a valid subject");
+      } else {
+          printError("subjectErr", "");
+          nameErr = false;
+      }
+  }
+  
+    // Validate comment
+  if(comment == "") {
+      printError("commentErr", "Please enter your name");
+  } else {
+      var regex = /^[a-zA-Z\s]+$/;                
+      if(regex.test(comment) === false) {
+          printError("commentErr", "Please enter a valid comment");
+      } else {
+          printError("commentErr", "");
+          nameErr = false;
+      }
+  }
+  
+  
+
+  // Prevent the form from being submitted if there are any errors
+  if((subjectErr && emailErr && commentErr ) == true) {
+     return false;
+  } else {
+      // Creating a string from input data for preview
+      var dataPreview = "You've entered the following details: \n" +
+                        "Subject : " + Subject + "\n" +
+                        "Comment : " + Comment + "\n" +
+                      
+      // Display input data in a dialog box before submitting the form
+      alert(dataPreview);
+  }
+}
+
