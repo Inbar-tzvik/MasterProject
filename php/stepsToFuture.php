c49fcc7c8fa1de8cbbd3244a41de4d73a3000631
Inbar-tzvik <inbar.tzvik@gmail.com>
2021-05-13 22:50:09 +0300

    Update stepsToFuture.php

--- php/stepsToFuture.php
+++ php/stepsToFuture.php
@@ -6,6 +6,8 @@ $database = "maycois_MasterProject";
 
 
 $conn = new mysqli($server_name, $username, $password, $database);
+$conn->set_charset('utf8');
+
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -30,15 +32,11 @@ if ($conn->query($sql) == FALSE) {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Result - Steps to the future</title>
-    
   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"-->
-  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -62,16 +60,13 @@ if ($conn->query($sql) == FALSE) {
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-
-
         <?php
-
           $bolsql_course = false;// הדיפולט הוא שלילי כלומר שהם לא עומדים בתנאי הקורס הזה 
           $bolHTML = false;
           $bolPYTHON = false;
           $bolUI_UX = false;
           $bolSAP_ERP= false;
-    
+
         $user_qry1 = "SELECT name_role,name_course,grade,description
         from Courses_roles INNER JOIN Courses ON Courses_roles.name_course=Courses.name where '".$name_role ."'=Courses_roles.name_role ";
         
@@ -98,16 +93,15 @@ if ($conn->query($sql) == FALSE) {
         }
         ?>
          <h1 class="text-center">צעדים לתפקיד הנחשק</h1 <br>
-     <p class="text-center"> בדף זה מוצגים לך הפערים בין הקורסים שלמדת לבין דרישות התפקיד שבחרת <br> הנתונים שמוצגים לך הם על סמך ביקוש השוק </p>
+         <p class="text-center"> בדף זה מוצגים לך הפערים בין הקורסים שלמדת לבין דרישות התפקיד שבחרת <br> הנתונים שמוצגים לך הם על סמך ביקוש השוק </p>
         
-       
-<?php
+         <?php
         if (empty(mysqli_fetch_assoc($user_res3))) {
          ?>
-             <br> <br> <br>                               <br> <br> <br>
+        <br> <br> <br>  <br> <br> <br>
 
         <p class="text-center"> אין תוצאות מתאימות </p>
-       <br>
+        <br>
         <br>
         <img src="../images/search.jpg" alt="" class="reveal img-responsive reveal-content image-center st_img bigger">
         <br>
@@ -136,81 +130,67 @@ if ($conn->query($sql) == FALSE) {
                       $bolUI_UX = true;
                     }
                     
-             elseif($row['name_course'] == 'SAP_ERP' && $SAP_ERP>= ((int)$row['grade'])){
+            elseif($row['name_course'] == 'SAP_ERP' && $SAP_ERP>= ((int)$row['grade'])){
                        $bolSAP_ERP = true;
-                       
                     }
-                   
                 }
-       
-                    
-            if($bolsql_course == true && $bolHTML == true && $bolPYTHON == true && $bolUI_UX == true &&  $bolSAP_ERP == true){
-              echo "אתה בדרך הנכונה ! יש לך את כל הציונים המתאימים בקורסים הנדרשים";
+        if($bolsql_course == true && $bolHTML == true && $bolPYTHON == true && $bolUI_UX == true &&  $bolSAP_ERP == true){
+                echo "<br> ";
+              echo " <p class='text-center'> אתה בדרך הנכונה ! יש לך את כל הציונים המתאימים בקורסים הנדרשים </p>";
                     }
+        else{
+                echo "<br> ";
+                echo "<p class='text-center'> הקורסים הבאים או שהציון שלך לא מספיק עבורם או שמומלץ שתעשה את קורסים אלו</p>";
+                 echo "<br> ";
+                 echo "<table dir='rtl' class='table table-bordered print '>";
+                       echo "<tr>";        
+                       echo "<th class='text-center'>שם הקורס</th>"; 
+                       echo  "<th class='text-center'>תיאור הקורס</th>";
+                       echo "</tr>"; 
+            if($bolsql_course == false){
+                     
+                        echo  "<tr>";
+                        echo  "<td>  sql_course </td>"; 
+                        echo  "<td> $sql_coursedes </td>"; 
+                        echo "</tr>";
+                 }
+            if($bolHTML == false){   
                     
-                    // בעייתי  עכשיו לעשות ככה לכל הקורסים
-            elseif($bolsql_course == true && $bolHTML == true &&  $bolPYTHON == false && $bolUI_UX == false &&   $bolSAP_ERP == false){
-                echo " צריך לעבוד על הציון בקורסים הבאים:פייתון,ממשק משתמש וסאפ";
-                echo  $PYTHONdes;
-                    
-            
-                echo      $UI_UXdes;
-              
-                  echo    $SAP_ERPdes;
-            }
-                
-            elseif($bolsql_course == false && $bolHTML == true  &&  $bolPYTHON == true && $bolUI_UX == true && $bolSAP_ERP == true){
-                    echo "you need to get a better grade at SQL ";
-                    }
-            elseif($bolsql_course == true && $bolHTML == false   &&  $bolPYTHON == false && $bolUI_UX == false &&  $bolSAP_ERP == false){
-                    echo "you need to get a better grade at SQL and C# ";
-                    
-                    }
-                    
-            else{
-                 echo "SQL & HTML ALL OVER AGAIN";
-                    }
-                        
-                        
-                   
-                        //   echo           "<th>Name</th>"; 
-
-                       
-                        //    <?php echo  $user_data['name_role']; ?></p>
-
-                        <?php
-                        //  echo           "<th>Description</th>";
-
-                       // echo       "</tr>";
-                      //  echo    "</thead>";
-                      //  echo    "<tbody>";
-
-
-                        ?>
-<!--
-                        <br>
-                        <tr>
-
-                        
-                              <td><//?php echo $user_data['name']; ?></td> 
-                            <td style='font-weight:bold;font-size:20px'><?php //echo 'תיאור התפקיד ' ?></td>
-                            <td class='text-center' style='color:black'> <?php //echo  $user_data['name_course']; ?></p>
+                        echo  "<tr>";
+                        echo  "<td>  HTML </td>"; 
+                        echo  "<td> $HTMLdes </td>"; 
+                        echo "</tr>";
                             
-</td>
-
-                        </tr> -->
-
-            </div>
-  
+                 }
+            if($bolPYTHON == false){   
+                        echo  "<tr>";
+                        echo  "<td>  PYTHON </td>"; 
+                        echo  "<td> $PYTHONdes </td>"; 
+                        echo "</tr>";
+                            
+                 }
+            if($bolUI_UX == false){   
+                        echo  "<tr>";
+                        echo  "<td>  UI_UX </td>"; 
+                        echo  "<td> $UI_UXdes </td>"; 
+                        echo "</tr>";
+                            
+                 }
+            if($bolSAP_ERP == false){   
+                        echo  "<tr>";
+                        echo  "<td>  SAP_ERP </td>"; 
+                        echo  "<td> $SAP_ERPdes </td>"; 
+                        echo "</tr>";
+                 }
+                    }
+                        ?>
    <?php
 
                     }
-               
+           
     ?>
     </tbody>
-
     </table>
-
     <div class="text-center">
         <a href="user_data_print.php" class="btn btn-primary">Print</a>
     </div>
@@ -223,12 +203,9 @@ if ($conn->query($sql) == FALSE) {
     <div id="footer"></div>
 </footer>
 
-
-
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 <script defer src="../js/headerfooter.js"></script>
 
 </html>
-
