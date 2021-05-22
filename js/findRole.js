362c25abb38a319be67a72830b9941c444386fb6
mayco15 <maycohen155@gmail.com>
2021-05-22 13:36:43 +0300

    Update

--- js/findRole.js
+++ js/findRole.js
@@ -1,15 +1,48 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+// function myFunction() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("myBtn");
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less";
+//       moreText.style.display = "inline";
+//     }
+//   }
+
+
+
+
+      
+function animateFunction() {
+let dots = document.getElementById("myDiv");
+
+ if (dots.style.display === "none") {
+dots.style.display = "inline";
+btnText.innerHTML = "סגור"; 
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+btnText.innerHTML = "הסבר"; 
+moreText.style.display = "inline";
+}
+}
+
+function animateFunction1() {
+    let dots = document.getElementById("myDiv1");
+    
+     if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText1.innerHTML = "סגור"; 
+    moreText1.style.display = "none";
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
+    dots.style.display = "none";
+    btnText1.innerHTML = "הסבר"; 
+    moreText1.style.display = "inline";
     }
-  }
+    }
+    
